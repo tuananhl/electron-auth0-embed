fff379059a5a2db419263729d5fed9f22f3265ad
Clement <tuananhit.oct@gmail.com>
2025-01-05 09:00:54 +0700

    fix: replace got by fetch

--- src/adapters/authAPI.ts
+++ src/adapters/authAPI.ts
@@ -1,19 +1,19 @@
-import got from 'got';
 import { Adapter } from '../types';
 import { context } from '../utils';
 
 const authAPIAdapter: Adapter = (config) => {
   return context('authAPI', {
-    exchangeAuthCode: (authCode, pair) => got.post(`https://${config.auth0.domain}/oauth/token`, {
-      json: {
+    exchangeAuthCode: (authCode, pair) => fetch(`https://${config.auth0.domain}/oauth/token`, {
+      method: 'POST',
+      body: JSON.stringify({
         grant_type: 'authorization_code',
         client_id: config.auth0.clientId,
         code_verifier: pair.verifier,
         code: authCode,
         redirect_uri: `https://${config.auth0.domain}/result`
-      }
-    }).json(),
-    getProfile: (accessToken) => got(`https://${config.auth0.domain}/userinfo`, { headers: { 'Authorization': `Bearer ${accessToken}` } }).json()
+      }),
+    }).then((response: Response) => response.json()),
+    getProfile: (accessToken) => fetch(`https://${config.auth0.domain}/userinfo`, { headers: { 'Authorization': `Bearer ${accessToken}` } }).then((response) => response.json())
   })
 };
 
